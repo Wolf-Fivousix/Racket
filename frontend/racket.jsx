274e1139962620986c39643d2cd7981487b84ef4
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-11-05 10:31:16 -0800

    Add entry file

---
+++ frontend/racket.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  const store = createStore();
+
+  ReactDOM.render(<h1>YAAAAY =D</h1>, root);
+});
