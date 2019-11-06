6364d8b381be2c4b90f3aaf56eacc948dca4dd7e
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-11-06 09:38:49 -0800

    Add Backend Authentication

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,25 @@
+class Api::SessionsController < ApplicationController
+  def create
+    user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password]
+    )
+
+    if user
+      login(user)
+      render json: user
+    else
+      render json: "Username and/or Password invalid", status: 404
+    end
+
+  end
+
+  def destroy
+    if current_user
+      logout
+      render {}
+    else
+      render json: "No user logged in", status: 404
+    end
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+class User < ApplicationRecord
+  validates :username, :email, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 1 }, allow_nil: true
+
+  attr_reader :password
+  
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    @user = User.find_by(username: username)
+    @user && @user.is_password?(password) ? @user : nil
+  end
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+end
