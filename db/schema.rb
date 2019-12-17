839bb4b24c94aba21aa7ba2dcddfcbad945b9404
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-12-17 07:34:50 -0800

    Add channel schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_222943) do
+ActiveRecord::Schema.define(version: 2019_12_17_004429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "channels", force: :cascade do |t|
+    t.integer "server_id", null: false
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "servers", force: :cascade do |t|
     t.integer "owner_id", null: false
