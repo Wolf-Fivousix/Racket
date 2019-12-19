c807d0f0da2651adf672cf28122a76316d125f22
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-12-18 20:08:06 -0800

    Add channels backend

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -9,11 +9,22 @@ class Api::ChannelsController < ApplicationController
     end
 
     def create
+        @channel = Channel.new(channel_params)
+
+        if @channel.save
+            render :channel
+        else
+            render json: @channel.errors.full_messages, status: 404
+        end
     end
 
     def update
     end
 
     def destroy
+    end
+
+    def channel_params
+        params.require(:channel).permit(:server_id, :title)
     end
 end
