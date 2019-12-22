c7b6f2823b11618f896e68fb325a19eb63c0eb61
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-12-21 20:37:57 -0800

    Add destroy action

--- frontend/actions/channel_actions.js
+++ frontend/actions/channel_actions.js
@@ -2,6 +2,7 @@ import * as channelUtils from "../util/channel_util";
 export const CREATE_CHANNEL = "CREATE_CHANNEL";
 export const RECEIVE_CHANNEL_ERRORS = "RECEIVE_CHANNEL_ERRORS";
 export const RECEIVE_SERVER_CHANNELS = "RECEIVE_SERVER_CHANNELS";
+export const DESTROY_CHANNEL = "DESTORY_CHANNEL";
 
 const createNewChannel = channel => ({
     type: CREATE_CHANNEL,
@@ -18,6 +19,11 @@ const receiveServerChannels = channels => ({
     channels
 });
 
+const deleteChannel = channelId => ({
+    type: DESTROY_CHANNEL,
+    channelId
+});
+
 export const createChannel = channel => dispatch => (
     channelUtils.createChannel(channel)
         .then((channel) => dispatch(createNewChannel(channel)))
@@ -27,5 +33,11 @@ export const createChannel = channel => dispatch => (
 export const getAllChannels = serverId => dispatch => (
     channelUtils.getAllChannels(serverId)
         .then((channels) => dispatch(receiveServerChannels(channels)))
+        .fail((errors) => dispatch(receiveChannelErrors(errors)))
+);
+
+export const destroyChannel = channelId => dispatch => (
+    channelUtils.destroyChannel(channelId)
+        .then((channelId) => dispatch(deleteChannel(channelId)))
         .fail((errors) => dispatch(receiveChannelErrors(errors)))
 );
