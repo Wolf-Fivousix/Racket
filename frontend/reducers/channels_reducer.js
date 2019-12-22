85ccc93a3744c459a3974d03927b719b964eb37f
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-12-21 20:40:21 -0800

    Add handling for remove channels

--- frontend/reducers/channels_reducer.js
+++ frontend/reducers/channels_reducer.js
@@ -1,6 +1,7 @@
 import {
     CREATE_CHANNEL,
-    RECEIVE_SERVER_CHANNELS
+    RECEIVE_SERVER_CHANNELS,
+    DESTROY_CHANNEL
 } from "../actions/channel_actions";
 
 const channelsReducer = (state = {}, action) => {
@@ -12,6 +13,11 @@ const channelsReducer = (state = {}, action) => {
 
         case RECEIVE_SERVER_CHANNELS:
             return Object.assign({}, action.channels);
+
+        case DESTROY_CHANNEL:
+            let newState = Object.assign({}, state);
+            delete newState[action.channelId];
+            return newState;
 
         default:
             return state;
