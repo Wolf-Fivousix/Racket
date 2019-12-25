26186c31dc60358a2b596c535e3713f47536803a
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-12-25 12:01:55 -0800

    Add inputs to modal

--- frontend/components/channel/channel_options.jsx
+++ frontend/components/channel/channel_options.jsx
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ChannelOptions(props) {
-   return(
-       <div className="channelModal">
+    const [channeLName, setChannelName] = useState("");
+
+    function handleNameChange(e) {
+        setChannelName(e.target.value);
+    }
+
+    function createChannel() {
+        console.log(channeLName);
+        props.createChannel({
+            title: "Beep Boop",
+            server_id: Number(props.serverId)
+        });
+    }
+
+    return(
+        <div className="channelModal">
+            <p>New Channel Name:</p>
             <input 
-                className="" 
+                className="channelNameInputField defaultInput" 
                 type="text"
+                onChange={handleNameChange}
             />
             <button
-                className="button"
+                className="createChannelButton button"
+                onClick={createChannel}
                 >
                 Create Channel
             </button>
-       </div>
-   ); 
+        </div>
+    ); 
 };
