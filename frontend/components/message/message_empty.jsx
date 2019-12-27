49bcc36fb6f7662e292ddaf623399e9eab4b184c
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-12-27 15:28:54 -0800

    Fix default styling for empty channel

--- frontend/components/message/message_empty.jsx
+++ frontend/components/message/message_empty.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function MessageEmpty(pros) {
     return (
-        <div className="chat">
-            <h1>Server messages</h1>
+        <div className="noChannelSelected">
+            <h1>You find yourself in a strange place. You don't have access to any text channels, or there are none in this server.</h1>
         </div>
     );
 }
