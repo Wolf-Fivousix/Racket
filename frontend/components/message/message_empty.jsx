a18372fb14c8e8ef6224bb8e3af8efbe1be2d1e4
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-12-27 15:34:09 -0800

    Fix default image for no channel

--- frontend/components/message/message_empty.jsx
+++ frontend/components/message/message_empty.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function MessageEmpty(pros) {
     return (
         <div className="noChannelSelected">
+            <img className="noChannelImage" src="https://discordapp.com/assets/94b5558353f1d01035a874f6eddf6d70.svg" alt=""/>
             <h1>You find yourself in a strange place. You don't have access to any text channels, or there are none in this server.</h1>
         </div>
     );
--- frontend/components/message/message_index.jsx
+++ frontend/components/message/message_index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function MessageIndex(props) {
     return (
         <div className="chat">
-                <h1>GET RAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACKET</h1>
+            <h1>GET RAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACKET</h1>
         </div>
     );
 };
