e60096022ea289183b1ad9e34d4185dcd392d518
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-12-27 18:40:27 -0800

    Implement displaying of messages

--- frontend/actions/message_actions.js
+++ frontend/actions/message_actions.js
@@ -1,14 +1,14 @@
 import * as messageUtils from "../util/message_util";
 export const RECEIVE_CHANNEL_MESSAGES = "RECEIVE_CHANNEL_MESSAGES";
 
-const receiveChannelMessages = channelId => ({
+const receiveChannelMessages = messages => ({
     type: RECEIVE_CHANNEL_MESSAGES,
-    channelId
+    messages
 });
 
 export const getAllMessages = channelId => dispatch => (
     messageUtils.getAllMessages(channelId)
-        .then((res) => console.log("done: ", res))
+        .then(messages => dispatch(receiveChannelMessages(messages)))
         .fail(errors => console.log(errors))
         // .fail((errors) => dispatch(receiveChannelErrors(errors)));
 );
--- frontend/components/message/message_index.jsx
+++ frontend/components/message/message_index.jsx
@@ -4,16 +4,23 @@ import useReactRouter from "use-react-router";
 import { getAllMessages } from "../../actions/message_actions";
 
 export default function MessageIndex(props) {
+    const messages = useSelector(state => state.entities.messages);
     const dispatch = useDispatch();
     const { history, match, location } = useReactRouter();
 
     useEffect(() => {
         dispatch(getAllMessages(match.params.channelId));
-    });
+    }, [match.params.channelId]);
+
+    const messagesList = Object.values(messages).map((message, index) =>
+        <p>{message.body}</p>
+    );
+
 
     return (
         <div className="chat">
-            <h1>GET RAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACKET</h1>
+            <h1>Messages...</h1>
+            {messagesList}
         </div>
     );
 };
