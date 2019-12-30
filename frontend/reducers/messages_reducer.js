f33bc7fd3041bb8387d75a60af0be84d5ad40787
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-12-30 15:40:25 -0800

    Create messages reducer

--- frontend/reducers/messages_reducer.js
+++ frontend/reducers/messages_reducer.js
@@ -1,5 +1,6 @@
 import {
-    RECEIVE_CHANNEL_MESSAGES
+    RECEIVE_CHANNEL_MESSAGES,
+    RECEIVE_NEW_MESSAGE
 } from "../actions/message_actions";
 
 const messagesReducer = (state = {}, action) => {
@@ -9,6 +10,9 @@ const messagesReducer = (state = {}, action) => {
         case RECEIVE_CHANNEL_MESSAGES:
             return Object.assign({}, action.messages);
 
+        case RECEIVE_NEW_MESSAGE:
+            return Object.assign({}, state, action.message);
+            
         default:
             return state;
     }
