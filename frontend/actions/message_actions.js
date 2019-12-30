5f19d685b277ad2abc11cd2d216f9a4a3343e35b
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-12-30 15:40:11 -0800

    Create message actions

--- frontend/actions/message_actions.js
+++ frontend/actions/message_actions.js
@@ -1,9 +1,15 @@
 import * as messageUtils from "../util/message_util";
 export const RECEIVE_CHANNEL_MESSAGES = "RECEIVE_CHANNEL_MESSAGES";
+export const RECEIVE_NEW_MESSAGE = "RECEIVE_NEW_MESSAGE";
 
 const receiveChannelMessages = messages => ({
     type: RECEIVE_CHANNEL_MESSAGES,
     messages
+});
+
+const receiveNewMessage = message => ({
+    type: RECEIVE_NEW_MESSAGE,
+    message
 });
 
 export const getAllMessages = channelId => dispatch => (
@@ -11,4 +17,10 @@ export const getAllMessages = channelId => dispatch => (
         .then(messages => dispatch(receiveChannelMessages(messages)))
         .fail(errors => console.log(errors))
         // .fail((errors) => dispatch(receiveChannelErrors(errors)));
+);
+
+export const createMessage = message => dispatch => (
+    messageUtils.createMessage(message)
+        .then(message => dispatch(receiveNewMessage(message)))
+        .fail(errors => console.log(errors))
 );
