d4f3e7fdb57c13a3d9f771b8c1be78bf216fcfc7
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2019-12-31 18:35:59 -0800

    Implement membership creation

--- app/controllers/api/memberships_controller.rb
+++ app/controllers/api/memberships_controller.rb
@@ -1,3 +1,22 @@
 class Api::MembershipsController < ApplicationController
-    
+    def index
+    end
+
+    def create
+        @membership = Membership.new(membership_params)
+        @membership.member_id = current_user.id
+
+        if @membership.save
+            render json: @membership
+        else
+            render json: @membership.errors.full_messages, status: 404
+        end
+    end
+
+    def destroy
+    end
+
+    def membership_params
+        params.require(:membership).permit(:server_id)
+    end
 end
