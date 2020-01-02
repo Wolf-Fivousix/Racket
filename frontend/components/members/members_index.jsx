f933cd9487592183e087033ef5a23250b9013e2b
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-01-01 19:13:35 -0800

    Implement working version of members

--- frontend/components/members/members_index.jsx
+++ frontend/components/members/members_index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 export default function MembersIndex() {
+    const members = Object.values(useSelector(state => state.entities.memberships))
+                        .map((member, index) =>
+                            <li key={index}>{member.member_id}</li>
+                        );
+
     return (
         <ul className="membersList">
-            <li>Tchuruuu</li>
+            {members}
         </ul>
     );
 }
