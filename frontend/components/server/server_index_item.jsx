44de6a0ccfa93eea336f8db9d2b0bd8ee643a6f7
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-01-06 12:23:23 -0800

    Refactor server selection styling
      This solution will work by removing the "selectedServer" string from the class name,
    generalizing it to all buttons. Unfortunately the system buttons are NOT implemented
    as part of the serverIndexItem component. They have to be aligned with it in order
    for this update to work on them by default.

--- frontend/components/server/server_index_item.jsx
+++ frontend/components/server/server_index_item.jsx
@@ -6,18 +6,22 @@ export default function ServerIndexItem(props) {
     const { history, location } = useReactRouter();
 
     function handleClick(e) {
-        let servers = document.getElementsByClassName("serverIndexItemButton");
-        for (let i = 0; i < servers.length; ++i) {
-            servers[i].className="serverIndexItemButton";
-        }
+        removeSelectedClass();
         e.target.className += " selectedServer";
 
         const address = `/servers/${server.id}`;
         if (!location.pathname.includes(address)) history.push(address);
     }
 
-    // if (location.pathname.includes(address)) console.log(document.getElementById(server.id));
-    // document.getElementById(server.id).classList.add("selectedServer");
+    function removeSelectedClass() {
+        let servers = document.getElementsByClassName("serverIndexItemButton");
+        for (let i = 0; i < servers.length; ++i) {
+            if(servers[i].className.includes(" selectedServer")) {
+                servers[i].className = servers[i].className.replace(" selectedServer", "");
+            }
+        }
+    }
+
     return(
         <div className="serverIndexItemBox" id={server.id}>
             <button
