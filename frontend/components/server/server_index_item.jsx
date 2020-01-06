418513b33ac9ddf1ef78e2614d33c00310f6ee6c
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-01-06 12:06:55 -0800

    Create half working version of server button selection

--- frontend/components/server/server_index_item.jsx
+++ frontend/components/server/server_index_item.jsx
@@ -5,13 +5,21 @@ export default function ServerIndexItem(props) {
     const server = props.server;
     const { history, location } = useReactRouter();
 
-    function handleClick() {
+    function handleClick(e) {
+        let servers = document.getElementsByClassName("serverIndexItemButton");
+        for (let i = 0; i < servers.length; ++i) {
+            servers[i].className="serverIndexItemButton";
+        }
+        e.target.className += " selectedServer";
+
         const address = `/servers/${server.id}`;
         if (!location.pathname.includes(address)) history.push(address);
     }
 
+    // if (location.pathname.includes(address)) console.log(document.getElementById(server.id));
+    // document.getElementById(server.id).classList.add("selectedServer");
     return(
-        <div className="serverIndexItemBox">
+        <div className="serverIndexItemBox" id={server.id}>
             <button
                 className="serverIndexItemButton"
                 onClick={handleClick}>
