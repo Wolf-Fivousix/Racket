d69a864d45133d8c365e0210da2765cd4b60a7a6
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-01-07 12:26:36 -0800

    Implement message styling with divisor

--- frontend/components/message/message_index.jsx
+++ frontend/components/message/message_index.jsx
@@ -15,12 +15,16 @@ export default function MessageIndex(props) {
     }, [match.params.channelId]);
 
     const messagesList = Object.values(messages).map((message, index) =>
-        <article className="textMessage" key={index}>
-            <p className="author">{`${message.author}: `}</p>
-            <p>{message.body}</p>
-        </article>
-    );
+        <div className="textMessage" key={index}>
+            <h2 className="messageHeader">
+                <p className="author">{`${message.author}`}</p>
+                <time className="timeStamp">{`${message.created_at}`}</time>
+            </h2>
 
+            <p className="messageBody">{message.body}</p>
+            <div className="messageDivider"></div>
+        </div>
+    );
 
     return (
         <div className="chat">
