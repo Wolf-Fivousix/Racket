4c6d5f33b618030937234a79f74eaabc64d1ccfe
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-01-09 14:15:48 -0800

    Fix home server display page

--- frontend/components/server/temporary_component.jsx
+++ frontend/components/server/temporary_component.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 const temporaryComponent = () => (
     // {/* This will go inside the next component. */ }
-    <div>
+    <div className="content">
         <div className="channelList" >
             <h1 className="serverNameHeader">
                 Home Page
             </h1>
-        </div >
+        </div>
         <div className="chat">
             <h1>PrivateMessages Display</h1>
             <h2>Get RACKEEETTTT! =D</h2>
