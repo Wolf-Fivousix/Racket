a114114384647d6c90eae054f5c0ac732f2045ff
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-01-13 12:40:08 -0800

    Implement long name cropping

--- frontend/components/members/members_index.jsx
+++ frontend/components/members/members_index.jsx
@@ -4,9 +4,9 @@ import { useSelector } from "react-redux";
 export default function MembersIndex() {
     const members = Object.values(useSelector(state => state.entities.memberships))
                         .map((member, index) =>
-                            <li className="memberName" key={index}>{member.name}</li>
+                            <li className="memberName" key={index}>{member.name.slice(0, 19)}</li>
                         );
-
+ 
     return (
         <ul className="membersList">
             {members}
