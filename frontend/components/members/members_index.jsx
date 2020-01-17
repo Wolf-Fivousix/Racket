0f4b7ec656bb4c647f0f25c64c38b3a8c94cbe49
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-01-17 13:30:15 -0800

    Style avatar image component

--- frontend/components/members/members_index.jsx
+++ frontend/components/members/members_index.jsx
@@ -5,8 +5,10 @@ import AvatarImage from "./avatar_img";
 export default function MembersIndex() {
     const members = Object.values(useSelector(state => state.entities.memberships))
                         .map((member, index) =>
-                            // <li className="memberName" key={index}>{member.name.slice(0, 19)}</li>
-                            <AvatarImage />
+                            <li className="memberPlate">
+                                <AvatarImage />
+                                <p className="memberName" key={index}>{member.name.slice(0, 19)}</p>
+                            </li>
                         );
  
     return (
