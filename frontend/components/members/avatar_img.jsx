af77e72887177df4bcb6c9755c7d3cec682d5f95
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-01-17 12:42:13 -0800

    Create avatar component

---
+++ frontend/components/members/avatar_img.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function AvatarImage() {
+
+    return (
+        <img className="avatarImage" src="https://discordapp.com/assets/6debd47ed13483642cf09e832ed0bc1b.png" alt=""/>
+    );
+}
