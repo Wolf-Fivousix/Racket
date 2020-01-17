235730d3c4535a15f48e915b55d6afc9d2589681
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-01-17 13:37:37 -0800

    Fix same key error

--- frontend/components/members/avatar_img.jsx
+++ frontend/components/members/avatar_img.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
-export default function AvatarImage() {
-
+export default function AvatarImage(props) {
+    
     return (
         <img className="avatarImage" src="https://discordapp.com/assets/6debd47ed13483642cf09e832ed0bc1b.png" alt=""/>
     );
--- frontend/components/members/members_index.jsx
+++ frontend/components/members/members_index.jsx
@@ -3,11 +3,12 @@ import { useSelector } from "react-redux";
 import AvatarImage from "./avatar_img";
 
 export default function MembersIndex() {
+    
     const members = Object.values(useSelector(state => state.entities.memberships))
                         .map((member, index) =>
-                            <li className="memberPlate">
-                                <AvatarImage />
-                                <p className="memberName" key={index}>{member.name.slice(0, 14) + `${member.name.length > 14 ? "..." : ""}`}</p>
+                            <li className="memberPlate" key={index}>
+                                <AvatarImage id={member.member_id % 10}/>
+                                <p className="memberName">{member.name.slice(0, 14) + `${member.name.length > 14 ? "..." : ""}`}</p>
                             </li>
                         );
  
