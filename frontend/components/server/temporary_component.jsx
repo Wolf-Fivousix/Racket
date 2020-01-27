e5d84d3251e6c731bc68a8de2c9c9c11750f34a4
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-01-27 15:22:55 -0800

    Style temporary component with welcome message

--- frontend/components/server/temporary_component.jsx
+++ frontend/components/server/temporary_component.jsx
@@ -3,13 +3,14 @@ import React from "react";
 const temporaryComponent = () => (
     <div className="content">
         <div className="channelList" >
-            <h1 className="serverNameHeader">
+            <h1 className="serverNameHeader homePageMenuHeader">
                 Home Page
             </h1>
         </div>
-        <div className="chat">
-            <h1>PrivateMessages Display</h1>
-            <h2>Get RACKEEETTTT! =D</h2>
+        <div className="chat noChat">
+            <div className="walrusGameImage"></div>
+            <p className="welcomeMessage">Welcome to RACKET!!</p>
+            <p className="welcomeMessage">The #1 application for chatting and memes alike! Create or Join a server and have fun. =)</p>
         </div>
     </div>
 );
