3a18e1b98cf9f9231a5a86d045ca6f9b7f1d5939
Diego 'Wolf' Francisco <Fivousix@gmail.com>
2020-01-27 10:54:38 -0800

    Add styling for no channels component

--- frontend/components/message/message_empty.jsx
+++ frontend/components/message/message_empty.jsx
@@ -4,7 +4,7 @@ export default function MessageEmpty(pros) {
     return (
         <div className="noChannelSelected">
             <img className="noChannelImage" src="https://discordapp.com/assets/94b5558353f1d01035a874f6eddf6d70.svg" alt=""/>
-            <h1>You find yourself in a strange place. You don't have access to any text channels, or there are none in this server.</h1>
+            <h1 className="noChannelText">You find yourself in a strange place. You don't have access to any text channels, or there are none in this server.</h1>
         </div>
     );
 }
